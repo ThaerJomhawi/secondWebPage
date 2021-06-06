29067364f7cae7c046f57b02b5c2139fa12bb410
Thaer AlJomhawi <thaer.fjomhawi@gmail.com>
2021-06-06 17:44:59 +0300

    html structr, js code, css style

---
+++ js/app.js
@@ -0,0 +1,29 @@
+/*let userName = prompt('Please enter you name!');
+alert(` ${userName}, welcome to our website`);
+
+console.log(` ${userName}, welcome to our website`);
+
+let comName = prompt ('please enter your company name');
+alert (` ${userName} from ${comName}, thank you for being interested in our services`)
+
+console.log(` ${userName} from ${comName}, thank you for being interested in our services`);
+
+
+let fieldCom = prompt ('what is your company working field?');
+alert (` sure we have the best consultations for ${fieldCom} `)
+
+console.log(` ${fieldCom}, company working field `);
+
+let regcom = prompt ('where your company located ?');
+alert (` our office in  (${regcom})  region will do there best`)
+
+console.log(` ${regcom}, company location `);
+
+
+let mobNo = prompt ('please enter your contact Number ?');
+alert (` our team will contact you on (${mobNo})  as soon as possible`)
+
+console.log(` ${mobNo}, contact number `);
+
+alert (  `${userName}, from ${comName} its out vision to do consultaions for ${fieldCom} and other fields, in ${regcom} and any where in the world.  `)
+*/
